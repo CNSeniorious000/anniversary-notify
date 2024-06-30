b19fb9b5d3de23c57e6adb99d482653fda79e633
Muspi Merol <wjj123345567@icloud.com>
2024-06-30 20:06:46 +0800

    chore: use `round` instead of `int` for days delta calculating

--- src/logic/left.py
+++ src/logic/left.py
@@ -1,4 +1,4 @@
 def need_notify(name: str, total_seconds: int | float):
     hours = int(total_seconds / 3600)
-    print(f"- coming in {int(total_seconds) // 86400:>5} days: {name}")
+    print(f"- coming in {round(total_seconds / 86400):>5} days: {name}")
     return hours in (0, 1, 2, 8, 24, 24 * 2, 24 * 3, 24 * 4, 24 * 6, 24 * 8, 24 * 10, 24 * 14)
