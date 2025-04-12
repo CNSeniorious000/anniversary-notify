4c57c57c3f499cbdcba0dc5ecbe4191b9e02f045
Muspi Merol <me@promplate.dev>
2025-04-12 17:31:29 +0800

    feat: include a plain-text version of the email

--- src/utils/resend.py
+++ src/utils/resend.py
@@ -1,6 +1,7 @@
 from os import getenv
 
 import resend
+from html_text.html_text import extract_text
 
 from .default import get_default_signature
 from .log import print_markdown
@@ -13,7 +14,7 @@ signature = getenv("SIGNATURE") or get_default_signature()
 
 
 def send_raw(subject: str, html: str):
-    params: resend.Emails.SendParams = {"from": signature, "to": [to_email], "subject": subject, "html": html}
+    params: resend.Emails.SendParams = {"from": signature, "to": [to_email], "subject": subject, "html": html, "text": extract_text(html)}
 
     email = resend.Emails.send(params)
 
