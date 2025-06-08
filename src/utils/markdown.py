e15df3dbc0cc1ac311d762947f9948e7ba8f75d3
Muspi Merol <me@promplate.dev>
2025-06-09 03:40:15 +0800

    feat: support rendering markdown tables

--- src/utils/markdown.py
+++ src/utils/markdown.py
@@ -1,14 +1,6 @@
-from functools import cache
-
-
-@cache
-def get_markdown():
-    from mistune import create_markdown
-
-    return create_markdown(escape=False)
-
-
 def render(markdown: str) -> str:
-    renderer = get_markdown()
+    from mistune import html
 
-    return renderer(markdown)
+    print(html(markdown))
+
+    return html(markdown)  # type: ignore
